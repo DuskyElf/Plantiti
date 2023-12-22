beb18701fb06f6e0f72f1b2392563ca9b2b75744
DuskyElf <clayedison75@gmail.com>
2023-12-22 12:47:48 +0530

    bad changes

--- src/Plantiti/Main.java
+++ src/Plantiti/Main.java
@@ -5,19 +5,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String args[]) {
-        Game game = new Game(500);
-        Scanner scanner = new Scanner(System.in);
-
-        while (!game.isGameOver()) {
-            System.out.println("MENU");
-            Option.printOptions();
-
-            String choice = scanner.nextLine();
-
-            Option choiceOption = Option.getOption(choice);
-            choiceOption.runOption(game);
-        }
-
-        scanner.close();
+        System.out.println("bad changes");
     }
 }
